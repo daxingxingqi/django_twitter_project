e3111c91a6a74b9f21a798460461df6054ae5ee9
Zichen <zichenqi3@e163.com>
2022-09-30 11:53:46 -0400

    Login, Login Status, Logout, Signup Api

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,8 +1,60 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email')
+        fields = ('username', 'email')
+
+
+class LoginSerializer(serializers.Serializer):
+    # 检查username和password是否存在
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    # ModelSerializer可以把用户实际创建出来
+    # username和email希望大小写不敏感，但是serializers.CharField只会绝对对比
+    username = serializers.CharField(max_length=20, min_length=6)
+    password = serializers.CharField(max_length=20, min_length=6)
+    email = serializers.EmailField()
+
+    # 所以这里需要指定出来meta
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def validate(self, data):
+        # username和email希望大小写不敏感，所以输入的时候传入小写
+        if User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'message': 'This email address has been occupied.'
+            })
+        if User.objects.filter(email=data['email'].lower()).exists():
+            raise exceptions.ValidationError({
+                'message': 'This email address has been occupied.'
+            })
+        return data
+
+    def create(self, validated_data):
+        username = validated_data['username'].lower()  # 输入的时候传入小写
+        email = validated_data['email'].lower()
+        password = validated_data['password'] # 大小写敏感
+
+        # django定义好的
+        """
+        不能像如下这种传，因为加密了
+        User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+        """
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+        return user
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -4,7 +4,14 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
-from accounts.api.serializers import UserSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from accounts.api.serializers import UserSerializer, LoginSerializer, SignupSerializer
+from django.contrib.auth import (
+    authenticate as django_authenticate,
+    login as django_login,
+    logout as django_logout,
+)
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -14,3 +21,79 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class AccountViewSet(viewsets.ViewSet):
+    serializer_class = SignupSerializer
+
+    @action(methods=['GET'], detail=False)
+    # 查询登陆状态
+    # methods：只能用get去请求 detail：不用去object定义动作，如果detail==True,
+    # 地址会变成http://localhost:8000/api/accounts/1/login_status/
+    # GET请求会向数据库发索取数据的请求，从而来获取信息，该请求就像数据库的select操作一样，只是用来查询一下数据，
+    # 不会修改、增加数据，不会影响资源的内容，即该请求不会产生副作用。无论进行多少次操作，结果都是一样的
+    def login_status(self, request):
+        data = {'has_logged_in': request.user.is_authenticated}
+        if request.user.is_authenticated:
+            data['user'] = UserSerializer(request.user).data
+            # request.user 是一个object，使用UserSerializer可以把object变成json
+        return Response(data)  # status默认是200
+
+    @action(methods=['POST'], detail=False)
+    # 改变登陆状态
+    # POST请求同PUT请求类似，都是向服务器端发送数据的，但是该请求会改变数据的种类等资源，
+    # 就像数据库的insert操作一样，会创建新的内容。几乎目前所有的提交操作都是用POST请求的
+    def logout(self, request):
+        django_logout(request)
+        return Response({'success': True})
+
+    @action(methods=['POST'], detail=False)
+    def login(self, request):
+        """
+                默认的 username 是 admin, password 是222444
+        """
+        serializer = LoginSerializer(data=request.data)  # data是用户请求的数据 post-request.data，get-request.query_params
+        if not serializer.is_valid():  # 如果验证没通过(没有输入信息），返回400
+            return Response({
+                "success": False,
+                "message": "Please check input",
+                "errors": serializer.errors,  # 只要调用is_valid就有errors信息
+            }, status=400)
+        username = serializer.validated_data['username'] # validated_data：验证后对字符进行类型转换
+        password = serializer.validated_data['password']
+        # 如果用户不存在
+        if not User.objects.filter(username=username).exists():
+            return Response({
+                "success": False,
+                "message": "User does not exist"
+            }, status=400)
+
+        # 验证用户密码和用户名
+        user = django_authenticate(username=username, password=password)
+        if not user or user.is_anonymous:
+            return Response({
+                "success": False,
+                "message": "username and password does not match",
+            }, status=400)
+        # 调用login
+        django_login(request, user)
+        return Response({
+            "success": True,
+            "user": UserSerializer(instance=user).data,
+        })
+
+    @action(methods=['POST'], detail=False)
+    def signup(self, request):
+        serializer = SignupSerializer(data=request.data) # 如果传instance，就是更新。传数据就是创建，数据要加data
+        if not serializer.is_valid():  # 如果验证没通过，返回400
+            return Response({
+                "success": False,
+                "message": "Please check input",
+                "errors": serializer.errors,  # 只要调用is_valid就有errors信息
+            }, status=400)
+        user = serializer.save()
+        django_login(request, user)
+        return Response({
+            'success': True,
+            'user': UserSerializer(user).data,
+        }, status=201)
